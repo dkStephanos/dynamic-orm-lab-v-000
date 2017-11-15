a197211cd5ad102ef23305dff51dbcc22be2dfc3
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-15 15:40:12 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ class InteractiveRecord
   def self.find_by(value)
     sql = "SELECT * FROM #{self.table_name} WHERE name = '#{value[:name]}'"
     result = DB[:conn].execute(sql)
-    if result == nil
+    if result == []
       sql = "SELECT * FROM #{self.table_name} WHERE grade = '#{value[:grade]}'"
       result = DB[:conn].execute(sql)
       binding.pry
